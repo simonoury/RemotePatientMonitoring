cf443bf09172fb88c0aa1fed23a7b303cc66c622
simon <simonoury@gmail.com>
2019-11-28 10:15:01 +0000

    BlankBoard

--- src/main/java/BlankBoard.java
+++ src/main/java/BlankBoard.java
@@ -1,4 +1,13 @@
-public class BlankBoard {
+import javax.swing.*;
+import java.awt.*;
 
-    
+public class BlankBoard {
+    static GraphicsConfiguration gc;
+
+    public static void main (String[] args){
+        JFrame frame = new JFrame(gc);
+        frame.setSize(1500, 1000);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 }
