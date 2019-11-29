6c4f1c196c0496dc3cad2c026b7434ed15a8bfea
simon <simonoury@gmail.com>
2019-11-29 16:06:23 +0000

    SinWave

--- src/main/java/BlankBoard.java
+++ src/main/java/BlankBoard.java
@@ -1,15 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Graphics;
 
 public class BlankBoard extends JPanel {
     private JPanel mainpanel;
+    private JPanel graphECG;
+    private SinWave sinwave;
 
     public BlankBoard(){
+        mainpanel = new JPanel();
+        graphECG = new JPanel();
+        sinwave = new SinWave();
+        JLabel label1 = new JLabel("ECG:");
+
+        mainpanel.setLayout(new GridLayout(1, 1));
+        graphECG.setLayout(new GridLayout(5,1));
 
 
+        //JLabel label1 = new JLabel("ECG:");
+        //graphECG.add(label1);
+        graphECG.add(label1);
+        graphECG.add(sinwave);
+
+        mainpanel.add(graphECG);
     }
 
     public JPanel getMainpanel() {
         return mainpanel;
     }
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main (String[] args){
         BlankBoard board=new BlankBoard();
         JFrame frame = new JFrame("Remote Patient Monitoring");
-        frame.setSize(1500, 1000);
-        frame.add(board);
+        frame.setSize(1450, 9250);
+        frame.add(board.getMainpanel());
         frame.setVisible(true);
-        frame.setJMenuBar(new Menu());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
