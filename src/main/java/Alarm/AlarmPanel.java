2c9018c1daa37dbbc5e9974ac04b28b623ae9dc9
“constancedcgit config --global user.email “cwd17@ic.ac.uk <cwd17@ic.ac.uk>
2019-11-30 21:10:26 +0000

    VisualAlarm

--- src/main/java/Alarm/AlarmPanel.java
+++ src/main/java/Alarm/AlarmPanel.java
@@ -7,8 +7,12 @@ import java.awt.*;
 public class AlarmPanel extends JPanel {
     private JPanel alarmpanel= new JPanel();
     private FlowLayout grid;
-    private JLabel label = new JLabel();
-    private HRalarm alarm=new HRalarm();
+    private JLabel label=new JLabel();
+    private JLabel label2 =new JLabel();
+    int[] HR_values={110,110};
+    private Alarm HR_alarm=new Alarm(65,100,60,120,HR_values);
+    private Alarm RR_alarm=new Alarm(35,80,25,100,HR_values);
+
 
 
     public AlarmPanel(){
@@ -18,8 +22,14 @@ public class AlarmPanel extends JPanel {
         label.setText("Heart Rate:");
         label.setOpaque(true);
         alarmpanel.add(label);
-        alarm.getAlarm().setOpaque(true);
-        alarmpanel.add(alarm.getAlarm());
+        HR_alarm.getAlarm().setOpaque(true);
+        alarmpanel.add(HR_alarm.getAlarm());
+
+        label2.setText("Respiratory Rate:");
+        label2.setOpaque(true);
+        alarmpanel.add(label2);
+        RR_alarm.getAlarm().setOpaque(true);
+        alarmpanel.add(RR_alarm.getAlarm());
 
 
 
