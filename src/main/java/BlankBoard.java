436dbe0786b5a7a3b1eac8aac91fa0c1d442dcb2
constancedc <cwd17@ic.ac.uk>
2019-12-03 18:35:40 +0000

    Clock

--- src/main/java/BlankBoard.java
+++ src/main/java/BlankBoard.java
@@ -4,13 +4,13 @@ import java.awt.*;
 public class BlankBoard extends JPanel {
     private JPanel mainpanel;
     private GridLayout gridmain;
+    private Clock clock;
 
     public BlankBoard(){
         mainpanel= new JPanel();
         gridmain=new GridLayout(2,2);
-
         mainpanel.setLayout(gridmain);
-
+        //.add(clock.getClock_panel());
         mainpanel.add(new JPanel());
         mainpanel.add(new JPanel());
 
--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -1,10 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
+import java.sql.Time;
 
 public class Clock {
-
+    private JPanel clock_panel;
+    public Time time;
+    private JLabel tlabel;
     public Clock(){
+        clock_panel=new JPanel();
+        clock_panel.setLayout(new GridLayout(1,1));
+        Clock clock= new Clock();
+        tlabel= new JLabel(String.valueOf(time.getTime()));
+        tlabel.setVisible(true);
+        clock_panel.add(tlabel);
 
     }
 
-
+    public JPanel getClock_panel() {
+        return clock_panel;
+    }
 }
